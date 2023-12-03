8708fcf70a13b84885da31d9f39048b506807bf0
Никита Ботов <botov164@gmail.com>
2023-12-03 03:02:55 +0300

    Add project files.

---
+++ ExamBookStore/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace ExamBookStore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MaxWidth = 300;
+            MaxHeight = 585;
+            MinHeight = 585;
+            MinWidth = 300;
+           // Rememb.Checked = Properties.Settings.Default.IsRemember;
+        }
+
+        private void Button_Registr_Click(object sender, RoutedEventArgs e)
+        {
+            string login = loginBox.Text.Trim();
+            string password = pwdBox.Password.Trim();
+            
+            
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.Save();
+        }
+    }
+}
