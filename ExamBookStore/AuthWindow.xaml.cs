5080630688bd688fde84a9978924f2c8ccce1d17
Никита Ботов <botov164@gmail.com>
2023-12-14 11:28:19 +0300

    HotFix

--- ExamBookStore/AuthWindow.xaml.cs
+++ ExamBookStore/AuthWindow.xaml.cs
@@ -20,13 +20,13 @@ namespace ExamBookStore
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AuthWindow : Window
     {
 
         AppContext db;
 
       
-        public MainWindow()
+        public AuthWindow()
         {
             InitializeComponent();
             MaxWidth = 300;
